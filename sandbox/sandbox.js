b9def68cb752d701947b84aaae91456c737e034a
Cody Thompson <cody.thompson22@gmail.com>
2017-05-13 23:14:56 -0700

    tris in clip space

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -7,14 +7,24 @@ window.gwgl = null;
 window.renderer = null;
 
 function sbload () {
+  document.body.style.margin = '0';
+  document.body.style.padding = '0';
+
   canvas = document.createElement('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.style.position = 'fixed';
+  canvas.style.left = '0';
+  canvas.style.top = '0';
   gwgl = canvas.getContext('webgl');
   shaders.init(gwgl);
   renderer = new Renderer({
     gl: gwgl,
     shader_program: shaders.basic_prog
   });
+  document.body.appendChild(canvas);
+  gwgl.clearColor(0, 0, 0, 1);
+  gwgl.clear(gwgl.COLOR_BUFFER_BIT);
+  renderer.draw();
 };
 window.addEventListener('load', sbload);
--- src/renderer.js
+++ src/renderer.js
@@ -4,29 +4,60 @@ var Renderer = constructor({
   fields: [
     'gl',
     'shader_program',
-    // 'scale'
   ],
   required: [
     'gl',
     'shader_program'
   ],
   defaults: {
-    // scale: new Float32Array([1,1,1]),
   },
   init: function (args) {
-    // this.scale[1] = 2;
-
     this.vert_arr = this.build_verts();
+    this.setup_buffers();
+    this.setup_attribs();
   },
 });
 Renderer.prototype.build_verts = function () {
   var vert_arr = new Float32Array([
-      0, 0, 0, //tl
-      0, 1, 0, //bl
-      1, 1, 0, //br
+      0, 0, 0, //m
+      0, -1, 0, //bm
+      1, -1, 0, //br
+
+      0, 0, 0, //m
+      0, 1, 0, //tm
+      -1, 1, 0, //bl
   ]);
 
   return vert_arr;
+};
+Renderer.prototype.setup_buffers = function () {
+  this.vert_buffer = this.gl.createBuffer();
+};
+Renderer.prototype.setup_attribs = function () {
+  this.vert_attrib = this.gl.getAttribLocation(this.shader_program, 'aVert');
+};
+
+Renderer.prototype.buffer_data = function () {
+  this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vert_buffer);
+  this.gl.bufferData(this.gl.ARRAY_BUFFER, this.vert_arr, this.gl.STATIC_DRAW);
+};
+Renderer.prototype.enable_attribs = function () {
+  this.gl.enableVertexAttribArray(this.vert_attrib);
+};
+Renderer.prototype.disable_attribs = function () {
+  this.gl.disableVertexAttribArray(this.vert_attrib);
+};
+Renderer.prototype.draw = function () {
+  this.buffer_data();
+  this.enable_attribs();
+
+  var gl = this.gl;
+  gl.useProgram(this.shader_program);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.vert_buffer);
+  gl.vertexAttribPointer(this.vert_attrib, 3, gl.FLOAT, false, 0, 0);
+  gl.drawArrays(gl.TRIANGLES, 0, this.vert_arr.length / 3);
+
+  this.disable_attribs();
 };
 
 module.exports = Renderer;
