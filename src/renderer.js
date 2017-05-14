20d23be3cf98e2b75671b67b738b7031f3a2339b
Cody Thompson <cody.thompson22@gmail.com>
2017-05-13 22:36:32 -0700

    renderer added function for building verts array

--- src/renderer.js
+++ src/renderer.js
@@ -2,19 +2,32 @@ var constructor = require('./constructor');
 
 var Renderer = constructor({
   fields: [
-    'scale'
+    'gl',
+    'shader_program',
+    // 'scale'
   ],
   required: [
     'gl',
     'shader_program'
   ],
   defaults: {
-    scale: new Float32Array([1,1,1]),
+    // scale: new Float32Array([1,1,1]),
   },
   init: function (args) {
-    this.scale[1] = 2;
-  }
+    // this.scale[1] = 2;
+
+    this.vert_arr = this.build_verts();
+  },
 });
+Renderer.prototype.build_verts = function () {
+  var vert_arr = new Float32Array([
+      0, 0, 0, //tl
+      0, 1, 0, //bl
+      1, 1, 0, //br
+  ]);
+
+  return vert_arr;
+};
 
 module.exports = Renderer;
 
